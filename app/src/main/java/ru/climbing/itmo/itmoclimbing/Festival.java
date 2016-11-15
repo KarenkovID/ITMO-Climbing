fd51f282a9aa1af173b9a0fcbf2b3d43f1fd4824
sofatitovass <sof_titova@rambler.ru>
2016-11-16 00:02:12 +0300

    Delete TextView

--- app/src/main/java/ru/climbing/itmo/itmoclimbing/Festival.java
+++ app/src/main/java/ru/climbing/itmo/itmoclimbing/Festival.java
@@ -1,9 +1,5 @@
 package ru.climbing.itmo.itmoclimbing;
 
-/**
- * Created by macbook on 15.11.16.
- */
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
--- app/src/main/java/ru/climbing/itmo/itmoclimbing/Routs.java
+++ app/src/main/java/ru/climbing/itmo/itmoclimbing/Routs.java
@@ -1,4 +1,5 @@
 package ru.climbing.itmo.itmoclimbing;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -6,12 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import ru.climbing.itmo.itmoclimbing.R;
-
-
-/**
- * Created by macbook on 15.11.16.
- */
-
 
 public class Routs extends Fragment {
 
