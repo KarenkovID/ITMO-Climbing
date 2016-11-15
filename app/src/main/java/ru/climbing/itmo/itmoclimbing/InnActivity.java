07479b86bdecaa8f083dfb3d730270085c1b7768
NatalyaSlepkova <slepkovanatal@mail.ru>
2016-11-15 21:42:09 +0300

    from Natasha with love

---
+++ app/src/main/java/ru/climbing/itmo/itmoclimbing/InnActivity.java
@@ -0,0 +1,16 @@
+package ru.climbing.itmo.itmoclimbing;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * Created by Наталия on 15.11.2016.
+ */
+
+public class InnActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in);
+    }
+}
