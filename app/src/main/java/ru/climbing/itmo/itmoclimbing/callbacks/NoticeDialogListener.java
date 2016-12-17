34525c5c4fbdcda94c08f9be145e1d5cb0623588
Karenkov Igor <karenkovigor@gmail.com>
2016-12-17 20:10:56 +0300

    A small steps in creating competition manager

---
+++ app/src/main/java/ru/climbing/itmo/itmoclimbing/callbacks/NoticeDialogListener.java
@@ -0,0 +1,9 @@
+package ru.climbing.itmo.itmoclimbing.callbacks;
+
+/**
+ * Created by Игорь on 17.12.2016.
+ */
+
+public interface NoticeDialogListener<T> {
+    void onDialogPositiveClick(T data);
+}
