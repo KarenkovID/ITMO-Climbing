4a055e454bf44eb4176071c88f5814498ca9705c
sofatitovass <sof_titova@rambler.ru>
2016-12-18 19:57:48 +0300

    DB for Routes in progress

--- app/src/main/java/ru/climbing/itmo/itmoclimbing/cache/routeCache/RoutesDBHelper.java
+++ app/src/main/java/ru/climbing/itmo/itmoclimbing/cache/routeCache/RoutesDBHelper.java
@@ -1,4 +1,4 @@
-package ru.climbing.itmo.itmoclimbing.cache.competitionsCache;
+package ru.climbing.itmo.itmoclimbing.cache.routeCache;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -6,37 +6,38 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import ru.climbing.itmo.itmoclimbing.cache.utils.DBCorruptionHandler;
 
-import static ru.climbing.itmo.itmoclimbing.cache.competitionsCache.CompetitionsCacheContract.CompetitionsCached.CREATE_COMPETITIONS_TABLE;
+import static ru.climbing.itmo.itmoclimbing.cache.routeCache.RouteCacheContract.RoutesCached.CREATE_ROUTES_TABLE;
 
 /**
  * Created by macbook on 16.12.16.
  */
 
-public class CompetitionsDBHelper extends SQLiteOpenHelper {
+public class RoutesDBHelper extends SQLiteOpenHelper {
 
     private static final String DB_FILE_NAME = "competitions.db";
-    private static volatile CompetitionsDBHelper instance;
+    private static volatile RoutesDBHelper instance;
     private Context context;
 
 
-    public CompetitionsDBHelper(Context context) {
-        super(context, DB_FILE_NAME, null,0, // TODO fix version
+    public RoutesDBHelper(Context context) {
+        super(context, DB_FILE_NAME, null, 0, // TODO fix version
                 new DBCorruptionHandler(context, DB_FILE_NAME));
     }
 
-    public static CompetitionsDBHelper getInstance(Context context) {
+    public static RoutesDBHelper getInstance(Context context) {
         if (instance == null) {
-            synchronized (CompetitionsDBHelper.class) {
+            synchronized (RoutesDBHelper.class) {
                 if (instance == null) {
-                    instance = new CompetitionsDBHelper(context);
+                    instance = new RoutesDBHelper(context);
                 }
             }
         }
         return instance;
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_COMPETITIONS_TABLE);
+        db.execSQL(CREATE_ROUTES_TABLE);
     }
 
     @Override
